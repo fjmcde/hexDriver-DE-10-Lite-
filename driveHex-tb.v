059212119ec499d39c50860694399a337d2f05e7
Frank McDermott <fjmcde@gmail.com>
2022-03-14 23:26:53 -0600

    --insert :qm
    :wq

--- driveHex-tb.v
+++ driveHex-tb.v
@@ -1,29 +1,31 @@
 //==================================//
-// Name:        | Frank McDermott	//
+// Name:        | Frank McDermott	  //
 // File:        | driveHex-tb.v     //
-// Assignment:  | Lab 7: #5     	//
-// Version:     | v1.1              //
+// Assignment:  | Lab 7: #5     	  //
+// Version:     | v1.2              //
 //==================================//
 
+`include "driveHex.v"
 
 // testbench to test the driveHex module
-module driveHex-tb;
+module driveHex_tb;
   reg [9:0] in;
-  wire [7:0] hex0;
-  wire [7:0] hex1;
-  wire [7:0] hex2;
-  wire [7:0] hex3;
-  wire [7:0] hex4;
   wire [7:0] hex5;
+  wire [7:0] hex4;
+  wire [7:0] hex3;
+  wire [7:0] hex2;
+  wire [7:0] hex1;
+  wire [7:0] hex0;
   
-  
-  //twosCompliment c0(in, out);
-  driveHex d0(in, hex0, hex1, hex2, hex3, hex4, hex5);
+  driveHex d0(in, hex5, hex4, hex3, hex2, hex1, hex0);
   
   initial begin
-    $monitor("in: %b, 0: %b, 1: %b, 2: %b, 3: %b, 4: %b, 5: %b", in, hex0, hex1, hex2, hex3, hex4, hex5);
     #10
-    in = 10'b1111111111;
+    in = 10'b0000000111; // change as needed
+    $monitor(
+      "in: %b, 5: %b, 4: %b, 3: %b, 2: %b, 1: %b, 0: %b", 
+      in, hex5, hex4, hex3, hex2, hex1, hex0
+      );
     $finish;
   end
     
--- driveHex.v
+++ driveHex.v
@@ -1,89 +1,115 @@
 //==================================//
-// Name:        | Frank McDermott	//
+// Name:        | Frank McDermott	  //
 // File:        | driveHex.v        //
-// Assignment:  | Lab 7: #5     	//
-// Version:     | v1.1              //
+// Assignment:  | Lab 7: #5     	  //
+// Version:     | v1.2              //
 //==================================//
 
-// module which takes 10-bit signal from DE-10 Lite FPGA switches
-// and drives the 6 hex displays. uses twos complement to display
-// signed and unsigned numbers.
+// NEED TO WORK OUT NEGATIVE
+// module which takes a 10-bit signal as input from DE-10 Lite FPGA 
+// switches and drives the 6 hex displays. uses twos complement to 
+// display a signed or unsigned hexidecimal number.
 module driveHex(
-    input [9:0] in,
-    output[7:0] hex0, hex1, hex2, hex3, hex4, hex5
-    );
+  input [9:0] in,
+  output[7:0] hex5, hex4, hex3, hex2, hex1, hex0
+  );
 
-    // wires and regs
-    wire [5:0] nib0 = bin[3:0];
-    wire [5:0] nib1 = bin[7:4];
-    wire [5:0] nib2 = bin[9:8];
-    wire [5:0] nib3 = 6'b111111; //OFF
-    wire [5:0] nib4 = 6'b111111; //OFF
-    wire [5:0] nib5 = 6'b111111; //OFF
-  	wire [11:0] bin;
-    
-    // check MSB on input signal
-    checkMSB check0(in, bin);
+  // wires and regs
+  wire [4:0] nib5 = 5'b10000; //OFF
+  wire [4:0] nib4 = 5'b10000; //OFF
+  wire [4:0] nib3 = 5'b10000; //OFF
+  wire [4:0] nib2 = {1'b0, bin10[9:8]};
+  wire [4:0] nib1 = {1'b0, bin10[7:4]};
+  wire [4:0] nib0 = {1'b0, bin10[3:0]};
 
-    // drive hex displays
-    hexEncode h0(nib0, hex0);
-    hexEncode h1(nib1, hex1);
-    hexEncode h2(nib2, hex2);
-    hexEncode h3(nib3, hex3);
-    hexEncode h4(nib4, hex4);
-    hexEncode h5(nib5, hex5);
+  // temp wires for intermediate input/output from modules
+  wire [9:0] bin10;
+  wire [4:0] temp1;
+  wire [4:0] temp2;
+  
+  // check MSB on input signal
+  checkSign check0(in, bin10);
+
+  // remove leading zeros
+  removeZeros r0(nib2, nib1, temp2, temp1);
+
+  // drive hex displays
+  hexEncode h5(nib5, hex5);
+  hexEncode h4(nib4, hex4);
+  hexEncode h3(nib3, hex3);
+  hexEncode h2(temp2, hex2);
+  hexEncode h1(temp1, hex1);
+  hexEncode h0(nib0, hex0);
 endmodule
 
-module checkMSB(input [9:0] in, output reg [11:0] out);
-  	wire [9:0] tc_wire;
-  	reg [11:0] temp;
+// WORKING CORRECTLY
+// checks the MSB of 10-bit input signal to determine whether to treat the input
+// as signed or unsigned. If MSB is low (0), then treat as unsigned.
+// If high (1) treat as signed.
+module checkSign(input [9:0] in, output reg [9:0] out);
+  wire [9:0] tc_wire;
+  reg [9:0] temp;
 
-    // create the hardware
-    twosCompliment tc0(in, tc_wire);
+  // create the hardware
+  twosCompliment tc0(in, tc_wire);
 
-    // give logic to the hardware
-	 always @(*) begin
-        // if high, treat as signed
-        if(in[9] == 1) begin
-          	temp[9:0] <= tc_wire;
-          	temp[11] <= 1;
-				out <= temp;
-        end
-        // else, treat as unsigned
-        else begin
-          	temp[9:0] <= in;
-				out <= temp;
-        end
-    end
+  // give logic to the hardware
+  always @(*) begin
+      // if high, treat as signed
+      if(in[9] == 1) begin
+          temp <= tc_wire;
+      end
+      // else, treat as unsigned
+      else begin
+          temp <= in;
+      end
+
+      out <= temp;
+  end
 endmodule
 
+// WORKING CORRECTLY
+// takes 10-bit input signal, calculates it's two's complement
+// then outputs the converted 10-bit signal
 module twosCompliment(input [9:0] in, output [9:0] out);
-    // invert input and add 1
-    assign out = ~in + 10'b0000000001;
+  // invert input and add 1
+  assign out = ~in + 10'b0000000001;
 endmodule
 
-module hexEncode(input [5:0] in, output reg [7:0] hex_out);
-    always @(in) begin
-        case(in)
-            6'b000000: hex_out = 8'b11000000; // 0
-            6'b000001: hex_out = 8'b11111001; // 1
-            6'b000010: hex_out = 8'b10100100; // 2
-            6'b000011: hex_out = 8'b10110000; // 3
-            6'b000100: hex_out = 8'b10011001; // 4
-            6'b000101: hex_out = 8'b10010010; // 5
-            6'b000110: hex_out = 8'b10000010; // 6
-            6'b000111: hex_out = 8'b11111000; // 7
-            6'b001000: hex_out = 8'b10000000; // 8
-            6'b001001: hex_out = 8'b10011000; // 9
-            6'b001010: hex_out = 8'b10001000; // A
-            6'b001011: hex_out = 8'b10000011; // b
-            6'b001100: hex_out = 8'b11000110; // C
-            6'b001101: hex_out = 8'b10100001; // d
-            6'b001110: hex_out = 8'b10000110; // E
-            6'b001111: hex_out = 8'b10001110; // F
-            6'b100000: hex_out = 8'b10111111; // -
-            6'b111111: hex_out = 8'b11111111; // OFF
-          	default: hex_out = 8'b10110110; // ---
-        endcase
-    end
+module removeZeros(input [4:0] nib2, nib1, output [4:0] out2, out1);
+
+  wire [4:0] zero = 5'b00000;
+  wire [4:0] off = 5'b10000;
+
+  assign out2 = (nib2 == zero) ? off : nib2;
+  assign out1 = ((nib2 == zero) && (nib1 == zero)) ? off : nib1;
+  
+endmodule
+
+// NEED TO WORK OUT NEGATIVE SIGN
+// takes 5-bit input signal and outputs the corresponding 8-bit signal
+// to be displayed as a hexidecimal character on a 7-segment hex display.
+// the MSB has been coded as a "flag" to turn off the hex display
+module hexEncode(input [4:0] in, output reg [7:0] hex_out);
+  always @(in) begin
+      case(in)
+          5'b00000: hex_out = 8'b11000000; // 0
+          5'b00001: hex_out = 8'b11111001; // 1
+          5'b00010: hex_out = 8'b10100100; // 2
+          5'b00011: hex_out = 8'b10110000; // 3
+          5'b00100: hex_out = 8'b10011001; // 4
+          5'b00101: hex_out = 8'b10010010; // 5
+          5'b00110: hex_out = 8'b10000010; // 6
+          5'b00111: hex_out = 8'b11111000; // 7
+          5'b01000: hex_out = 8'b10000000; // 8
+          5'b01001: hex_out = 8'b10011000; // 9
+          5'b01010: hex_out = 8'b10001000; // A
+          5'b01011: hex_out = 8'b10000011; // b
+          5'b01100: hex_out = 8'b11000110; // C
+          5'b01101: hex_out = 8'b10100001; // d
+          5'b01110: hex_out = 8'b10000110; // E
+          5'b01111: hex_out = 8'b10001110; // F
+          5'b10000: hex_out = 8'b11111111; // OFF
+      endcase
+  end
 endmodule
